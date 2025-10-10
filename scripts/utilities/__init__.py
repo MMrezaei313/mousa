ee712c4e36f6ece69adf5efb35c873403b367782
MMrezaei313 <mr3759959@gmail.com>
2025-10-10 01:26:18 -0700

    Create __init__.py

---
+++ scripts/utilities/__init__.py
@@ -0,0 +1,7 @@
+"""
+Utilities package for Mousa Trading Bot
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Mousa Trading Bot Team'
+__description__ = 'Utility scripts and helper functions for Mousa Trading Bot'
