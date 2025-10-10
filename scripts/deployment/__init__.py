dab1c3dfab890eb5f199c1d28342ce200422f9cd
MMrezaei313 <mr3759959@gmail.com>
2025-10-10 01:25:43 -0700

    Create __init__.py

---
+++ scripts/deployment/__init__.py
@@ -0,0 +1,7 @@
+"""
+Deployment package for Mousa Trading Bot
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Mousa Trading Bot Team'
+__description__ = 'Deployment and monitoring scripts for Mousa Trading Bot'
