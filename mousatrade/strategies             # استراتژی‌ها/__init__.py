c83fa996ba9278fece218d32f7befe91764624d5
MMrezaei313 <mr3759959@gmail.com>
2025-10-10 22:00:40 -0700

    Update __init__.py

--- mousatrade/strategies             # استراتژی‌ها/__init__.py
+++ mousatrade/strategies             # استراتژی‌ها/__init__.py
@@ -1,1 +1,30 @@
+"""
+Trading Strategies Module for Mousa Trading Bot
+"""
 
+__version__ = '1.0.0'
+__author__ = 'Mousa Trading Bot Team'
+__description__ = 'Advanced trading strategies implementation'
+
+from .base_strategy import BaseStrategy
+from .technical_strategies import (
+    MovingAverageCrossover,
+    RSIMeanReversion,
+    MACDStrategy,
+    BollingerBandsStrategy
+)
+from .ml_strategies import MLStrategy, RandomForestStrategy, XGBoostStrategy
+from .portfolio_strategies import PortfolioStrategy, RiskParityStrategy
+
+__all__ = [
+    'BaseStrategy',
+    'MovingAverageCrossover',
+    'RSIMeanReversion', 
+    'MACDStrategy',
+    'BollingerBandsStrategy',
+    'MLStrategy',
+    'RandomForestStrategy',
+    'XGBoostStrategy',
+    'PortfolioStrategy',
+    'RiskParityStrategy'
+]
