786a113465d814b659bed04d37554009912d223c
MMrezaei313 <mr3759959@gmail.com>
2025-10-10 23:12:54 -0700

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+"""
+Test Suite for Mousa Trading Bot
+Comprehensive testing for all modules and components
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Mousa Trading Bot Team'
+
+# Test categories
+__all__ = [
+    'test_strategies',
+    'test_optimization', 
+    'test_utils',
+    'test_data',
+    'test_web'
+]
